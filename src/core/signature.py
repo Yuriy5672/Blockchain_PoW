900f6ef115efe3f9aa799c46426e7199cc5a88ec
Yuriy5672 <yuriypestovsky@gmail.com>
2022-02-12 17:39:19 +0300

    Added test case: test1, test2; the generatePrivateKey function is implemented

--- src/core/signature.py
+++ src/core/signature.py
@@ -1,5 +1,3 @@
-from email import message
-from hashlib import sha256, sha3_256
 from ellipticcurve.ecdsa import Ecdsa
 from ellipticcurve.privateKey import PrivateKey, PublicKey
 
@@ -12,16 +10,45 @@ def verifySignature(message, signature, publicKey):
     pubKey = PublicKey.fromString(publicKey)
     return Ecdsa.verify(message, signature, pubKey)
 
-def getKeys():
-    privateKey = PrivateKey()
-    return [privateKey.toString(), privateKey.publicKey().toString()]
+def generatePrivateKey():
+    return PrivateKey().toString()
+
+def getPublickKey(privateKey):
+    return PrivateKey.fromString(privateKey).publicKey().toString()
+
 
 
 #Tests
-privKey = getKeys()[0] #private key
-pubKey = getKeys()[1] #publick key
-print('priv ' + privKey + ' pub ' + pubKey)
-mess = 'Hello world'
-sign = createSignature(privKey, mess)
-print('sign ' + sign._toString())
-print(verifySignature(mess, sign, pubKey))
+#Generate keys, create signature, verify signature
+def test1():
+    privKey = generatePrivateKey() #private key
+    pubKey = getPublickKey(privKey) #publick key
+    print('private key: ' + privKey)
+    print('publick key: ' + pubKey)
+    mess = 'Hello world!'
+    sign = Ecdsa.sign(mess, PrivateKey.fromString(privKey))
+    print('signature: ' + sign.toBase64())
+    print('signature verification: ' + str(Ecdsa.verify(mess, sign, PublicKey.fromString(pubKey))))
+
+#Create signature, verify signature
+def test2(privKey, message):
+    try:
+        #Set keys
+        privKey = PrivateKey.fromString(privKey)
+        pubKey = privKey.publicKey()
+        print('private key: ' + privKey.toString())
+        print('publick key: ' + pubKey.toString())
+        #create signature
+        sign = Ecdsa.sign(message, privKey)
+        print('signature: ' + sign.toBase64())
+        #verify signature
+        print('signature verification: ' + str(Ecdsa.verify(message, sign, pubKey)))
+    except Exception as e:
+        print(e)
+
+#test1()
+
+#test2('b066a18fd30b130d00300acc2e45f39cf08f9836bf1de06976448b83fa815b81', '{"val": "","lock": "","sig": "","txid": ""}')
+
+#print(generatePrivateKey())
+
